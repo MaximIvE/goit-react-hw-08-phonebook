be0cc32526e046edac85b90d31cbc214e3f70c41
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-09-16 18:46:09 +0300

    remove persist and use locale storage for items

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App/App.jsx';
 import './index.css';
 
-import { store, persistor } from "./redux/store";
-// import { store } from "./redux/store";
+// import { store, persistor } from "./redux/store";
+import { store } from "./redux/store";
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
 
         <App />
 
-    </PersistGate>
+    {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
